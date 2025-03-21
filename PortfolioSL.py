83ad5f7384b609df9f0f235d655a8f420f8ee551
samirhuf <samirhuf65@gmail.com>
2025-03-21 22:29:56 +0530

    Add files via upload

--- PortfolioSL.py
+++ PortfolioSL.py
@@ -12,13 +12,9 @@ total_gain=df_inventory['Total Gain'].sum()
 current_investment=df_inventory['Current Investment'].sum()
 df_time=pd.read_csv(file_time)
 st.header('Time Updated Till: '+str(df_time['Time'].iloc[0]))
-styled_df = df_inventory.style.set_table_styles([
-        {'selector': 'th', 'props': [('background-color', '#f0f2f6'), ('font-weight', 'bold')]},
-        {'selector': 'td', 'props': [('text-align', 'left')]},
-        {'selector': 'table', 'props': [('border-collapse', 'collapse'), ('width', '100%')]},
-        {'selector': 'th, td', 'props': [('border', '1px solid #ddd'), ('padding', '8px')]}
-    ])
-st.write(styled_df.round(2))
+def highlight_profit(s):
+    return ['background-color: green']*len(s) if s.LTP>200 else ['background-color: red']*len(s)
+st.dataframe(df_inventory.style.apply(highlight_profit, axis=1))
 st.markdown('<style>body{background-color: #f0f2f6;}</style>', unsafe_allow_html=True)
 st.subheader('Total Gain Today: ' + str(total_gain_today))
 st.subheader('Total Gain: ' + str(total_gain))

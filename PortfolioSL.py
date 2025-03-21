27bc44efb2ea1b6d7be770c58515c6434052a815
samirhuf <samirhuf65@gmail.com>
2025-03-21 23:12:17 +0530

    Add files via upload

--- PortfolioSL.py
+++ PortfolioSL.py
@@ -14,7 +14,7 @@ current_investment=df_inventory['Current Investment'].sum()
 df_time=pd.read_csv(file_time)
 st.write('Time Updated Till: '+str(df_time['Time'].iloc[0]))
 def highlight_profit(s):
-    return ['background-color: #4BB735']*len(s) if s.GainToday>0 else ['background-color: #A02525']*len(s)
+    return ['background-color: #ACD6A7']*len(s) if s.GainToday>0 else ['background-color: #DC6D6D']*len(s)
 df_styled = (df_inventory.style.
 format(precision=2).
 apply(highlight_profit, axis=1))
